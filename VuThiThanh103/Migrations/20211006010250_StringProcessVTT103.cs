90f3f1049850c533d0838e1bd970fb0b533e190d
VuThiThanh1103 <vuthanh2906@gmail.com>
2021-10-06 08:06:11 +0700

    Create_StringProcessVTT103_VTT103

---
+++ VuThiThanh103/Migrations/20211006010250_StringProcessVTT103.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VuThiThanh103.Migrations
+{
+    public partial class StringProcessVTT103 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ VuThiThanh103/Models/StringProcessVTT103.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VuThiThanh103.Models
+{
+    public class StringProcessVTT103
+    {
+        public string LowerToUpper { get; set; }
+        
+        public  string ToUpper { get; set; }
+    }
+}
